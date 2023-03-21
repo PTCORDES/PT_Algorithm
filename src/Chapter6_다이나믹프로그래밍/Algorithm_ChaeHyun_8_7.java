2cac709a047f2ebcc9ad3b72844c8d4af80c08f0
RyuChaeHyun <lch010201@gmail.com>
2023-03-21 19:30:01 +0900

    다이나믹 실전문제 1,2,3,4

---
+++ src/Chapter6_다이나믹프로그래밍/Algorithm_ChaeHyun_8_7.java
@@ -0,0 +1,24 @@
+package Chapter6_다이나믹프로그래밍;
+
+import java.util.Scanner;
+
+//타일
+//점화식 : a(i) = a(i-1) + 2 * a(i-2)
+public class Algorithm_ChaeHyun_8_7 {
+
+    public static int[] d = new int[1001];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        d[1] = 1;
+        d[2] = 3;
+        for (int i = 3; i <= n; i++) {
+            d[i] = (d[i - 1] + 2 * d[i - 2]) % 796796;
+        }
+
+        System.out.println(d[n]);
+    }
+
+}
